09851229635dcef26e444e416c35574b24e68ecb
= <bjoern.pfoster@gmail.com>
2019-03-09 21:50:26 +0100

    Added authentication service
    - Added service
    - Loaded JWT Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,16 +8,43 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { IonicStorageModule, Storage } from '@ionic/storage';
+import { JWT_OPTIONS, JwtModule } from '@auth0/angular-jwt';
+import { environment } from '../environments/environment';
+
+export function jwtOptionsFactory(storage) {
+    return {
+        tokenGetter: () => {
+            return storage.get('access_token');
+        },
+        whitelistedDomains: [environment.url]
+    };
+}
 
 @NgModule({
-  declarations: [AppComponent],
-  entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
-  ],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent],
+    entryComponents: [],
+    imports: [
+        BrowserModule,
+        IonicModule.forRoot(),
+        AppRoutingModule,
+        HttpClientModule,
+        IonicStorageModule.forRoot(),
+        JwtModule.forRoot({
+            jwtOptionsProvider: {
+                provide: JWT_OPTIONS,
+                useFactory: jwtOptionsFactory,
+                deps: [Storage],
+            }
+        }),
+    ],
+    providers: [
+        StatusBar,
+        SplashScreen,
+        {provide: RouteReuseStrategy, useClass: IonicRouteStrategy}
+    ],
+    bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+}
