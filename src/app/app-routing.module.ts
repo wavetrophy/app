474d09a2e6f986d7636b891d98bb3737e587fe2d
= <bjoern.pfoster@gmail.com>
2019-03-10 11:55:01 +0100

    Added Auth Module
    - Added login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,22 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './services/guards/auth.guard';
 
 const routes: Routes = [
-  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' }
+    {path: '', redirectTo: 'auth/login', pathMatch: 'full'},
+    {path: 'auth', loadChildren: './auth/auth.module#AuthModule'},
+    {
+        path: 'tabs',
+        loadChildren: './tabs/tabs.module#TabsPageModule',
+        canActivate: [AuthGuard],
+    },
 ];
+
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
-  exports: [RouterModule]
+    imports: [
+        RouterModule.forRoot(routes, {preloadingStrategy: PreloadAllModules}),
+    ],
+    exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule {
+}
