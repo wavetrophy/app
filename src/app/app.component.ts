fe65bcf575fff64fa07cc41f8e3473e4abaa4fce
= <bjoern.pfoster@gmail.com>
2019-03-10 12:17:08 +0100

    Added authentication state detection while booting the application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,24 +3,50 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { AuthService } from './services/auth/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  templateUrl: 'app.component.html'
+  templateUrl: 'app.component.html',
 })
+/**
+ * Class AppComponent
+ */
 export class AppComponent {
-  constructor(
+  /**
+   * AppComponent constructor.
+   * @param {Platform} platform The platform
+   * @param {SplashScreen} splashScreen The splash screen
+   * @param {StatusBar} statusBar The status bar
+   * @param {AuthService} authService The auth service
+   * @param {Router} router The router
+   */
+  public constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private authService: AuthService,
+    private router: Router,
   ) {
     this.initializeApp();
   }
 
-  initializeApp() {
+  /**
+   * Initialize the application.
+   */
+  public initializeApp() {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+
+      this.authService.authenticationState.subscribe(state => {
+        if (state) {
+          this.router.navigate(['tabs']);
+        } else {
+          this.router.navigate(['auth', 'login']);
+        }
+      });
     });
   }
 }
