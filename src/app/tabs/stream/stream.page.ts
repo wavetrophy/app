ffd8a1e219433e8ad0b66d7703fa690690909992
= <bjoern.pfoster@gmail.com>
2019-03-20 11:49:14 +0100

    Added loading animation to stream

--- src/app/tabs/stream/stream.page.ts
+++ src/app/tabs/stream/stream.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth/auth.service';
 import { StreamService } from '../../services/stream/stream.service';
+import { LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-stream',
@@ -9,16 +10,29 @@ import { StreamService } from '../../services/stream/stream.service';
 })
 export class StreamPage implements OnInit {
 
+  public errormessage = null;
+
   constructor(
     private auth: AuthService,
     private stream: StreamService,
+    private loading: LoadingController,
   ) {
   }
 
-  ngOnInit() {
-    const waveId = this.auth.user.user_id;
-    this.stream.getByUser(waveId).subscribe(res => {
+  async ngOnInit() {
+    const loader = await this.loading.create({
+      message: 'Loading',
+      spinner: 'crescent',
+    });
+    loader.present();
+
+    const userId = this.auth.user.user_id;
+    this.stream.getByUser(userId).subscribe(res => {
+      if (!res.success) {
+        this.errormessage = res.message;
+      }
       console.log(res);
+      loader.dismiss();
     });
   }
 
