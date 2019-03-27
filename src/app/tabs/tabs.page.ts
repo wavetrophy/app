6b022c53eddd559302dc159fd68982411ff8d634
= <bjoern.pfoster@gmail.com>
2019-03-27 11:06:51 +0100

    Updated location page to navigate properly with supertabs

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -3,14 +3,12 @@ import { StreamPage } from './stream/stream.page';
 import { Tab2Page } from '../tab2/tab2.page';
 import { Tab3Page } from '../tab3/tab3.page';
 
-// TODO change to @Component
-const typeDecorator = Component({
+
+@Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
   styleUrls: ['tabs.page.scss'],
-});
-
-@typeDecorator
+})
 export class TabsPage {
   public streamPage: any = StreamPage;
   public tab2Page: any = Tab2Page;
