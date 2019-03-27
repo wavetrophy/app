5ff5af76e50b868e2c2767b9a535e1ce6ac7ced4
= <bjoern.pfoster@gmail.com>
2019-03-27 10:58:56 +0100

    Introduced local variable to make the IDE happy

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -3,12 +3,14 @@ import { StreamPage } from './stream/stream.page';
 import { Tab2Page } from '../tab2/tab2.page';
 import { Tab3Page } from '../tab3/tab3.page';
 
-
-@Component({
+// TODO change to @Component
+const typeDecorator = Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
-  styleUrls: ['tabs.page.scss']
-})
+  styleUrls: ['tabs.page.scss'],
+});
+
+@typeDecorator
 export class TabsPage {
   public streamPage: any = StreamPage;
   public tab2Page: any = Tab2Page;
