71cba8c700c0d913fa3e2a982220723534f37ba1
= <bjoern.pfoster@gmail.com>
2019-04-24 22:33:33 +0200

    Added local notification service

---
+++ src/app/services/firebase/cloud-messaging/local-notification.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { ILocalNotification, LocalNotifications } from '@ionic-native/local-notifications/ngx';
+import { LocalNotification } from './local-notification';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LocalNotificationService {
+  /**
+   * LocalNotificationService
+   * @param {LocalNotifications} notifications
+   */
+  constructor(
+    private notifications: LocalNotifications,
+  ) {
+  }
+
+  /**
+   * Schedule a notification
+   */
+  public schedule(notification: LocalNotification) {
+    // TODO add icons
+    // TODO load defaults from a config
+    const defaults: ILocalNotification = {
+      // config
+      sound: 'res://platform_default',
+      led: '#3880ff', // primary
+      vibrate: true,
+      // defaults
+      priority: 2, // reallly important
+      icon: 'res://icon',
+      smallIcon: 'res://ic_popup_reminder',
+      launch: true,
+      wakeup: true,
+      autoClear: true,
+    };
+
+    const localNotification = Object.assign(defaults, notification);
+    this.notifications.schedule(localNotification);
+  }
+}
