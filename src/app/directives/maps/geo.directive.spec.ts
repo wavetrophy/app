8776917d2e688240e7eae8bbf1d68b5f3e255951
= <bjoern.pfoster@gmail.com>
2019-06-02 17:28:02 +0200

    Added geo directive

---
+++ src/app/directives/maps/geo.directive.spec.ts
@@ -0,0 +1,8 @@
+import { GeoDirective } from './geo.directive';
+
+describe('GeoDirective', () => {
+  it('should create an instance', () => {
+    const directive = new GeoDirective();
+    expect(directive).toBeTruthy();
+  });
+});
