9a592eb70a8f0b7cfb9dffe818fa9a20ee8dcb5d
= <bjoern.pfoster@gmail.com>
2019-06-14 10:48:49 +0200

    Maybe fixed tab bug

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
 import { StreamPage } from './stream/stream.page';
 import { MainOptionsPage } from '../popover/main-options/main-options.page';
 import { PopoverController } from '@ionic/angular';
 import { ContactsPage } from './contacts/contacts.page';
 import { FaqPage } from './faq/faq.page';
 import { HotelsPage } from './hotels/hotels.page';
+import { ConsoleLogger } from '../services/logger/logger';
 
-
+const logger = new ConsoleLogger('TABS');
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
@@ -18,9 +19,27 @@ export class TabsPage {
   public faqPage: any = FaqPage;
   public hotelsPage: any = HotelsPage;
 
-  public constructor(private popover: PopoverController) {
+  /**
+   * Constructor
+   * @param {PopoverController} popover
+   * @param {NgZone} ng
+   */
+  public constructor(private popover: PopoverController, private ng: NgZone) {
   }
 
+  /**
+   * Ion view will enter
+   */
+  public ionViewWillEnter(): void {
+    console.log('LOADED TABS');
+    this.ng.run(() => logger.info('Force reloaded tabs'));
+  }
+
+  /**
+   * Open options page
+   * @param event
+   * @return {Promise<any>}
+   */
   public async openOptions(event) {
     const popover = await this.popover.create({
       component: MainOptionsPage,
